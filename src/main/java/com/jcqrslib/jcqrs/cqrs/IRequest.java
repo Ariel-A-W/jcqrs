9aea9a687422e00e38cbd751f36f80721b2a275d
Ariel Alejandro Wagner <arqdev.arielwagner@gmail.com>
2025-07-23 12:24:43 -0300

    Revert "version 1.0.1 (23-07-2025)"

--- src/main/java/com/jcqrslib/jcqrs/cqrs/IRequest.java
+++ src/main/java/com/jcqrslib/jcqrs/cqrs/IRequest.java
@@ -1,8 +1,3 @@
 package com.jcqrslib.jcqrs.cqrs;
 
-/**
- * Interface for the requests that encapsulate the responses.
- * 
- * Interface para las solicitudes que encapsulan las respuestas.
- */
 public interface IRequest<TResponse> { }
